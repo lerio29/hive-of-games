ea07d749c3006d26cfdbf9758a9011d6ff768519
lerio29 <chicha29@free.fr>
2019-08-27 17:04:29 +0200

    add api steam service and interceptor rxjs

---
+++ src/app/api-interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (!req.headers.has('Content-Type')) {
+            req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
+        }
+
+        req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
+        return next.handle(req);
+    }
+
+}
--- src/app/steam-global-component/steam-global-component.component.ts
+++ src/app/steam-global-component/steam-global-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SteamApiService } from '../steam-api.service';
 
 @Component({
   selector: 'app-steam-global-component',
@@ -7,12 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SteamGlobalComponentComponent implements OnInit {
 
-  gameProvider: string = "steam";
+  public getOwnedGames: string;
+  
+  public gameProvider: string = "steam";
 
-  constructor() { 
+  constructor(private steamApi: SteamApiService) { 
+    private steamApi: SteamApiService;
   }
 
+
+
+
+// https://offering.solutions/blog/articles/2016/02/01/consuming-a-rest-api-with-angular-http-service-in-typescript/
+
   ngOnInit() {
+    
+        this.steamApi
+            .getOwnedGames<string>()
+            .subscribe((data: any[]) => this.values = data,
+            error => () => {
+                this.toasterService.pop('error', 'Damn', 'Something went wrong...');
+            },
+            () => {
+                this.toasterService.pop('success', 'Complete', 'Getting all values complete');
+                this.slimLoadingBarService.complete();
+            });
   }
 
 }
